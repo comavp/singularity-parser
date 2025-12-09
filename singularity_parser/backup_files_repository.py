90d69c4e3df453393e05944b70c3193ecc71c5a7
comavp <comavp@yandex.ru>
2025-12-09 23:02:45 +0400

    Improve DB layer for backup files
    * Added search by file_name and hash
    * Added removing old backup files

--- singularity_parser/backup_files_repository.py
+++ singularity_parser/backup_files_repository.py
@@ -26,6 +26,7 @@ class DatabaseManager:
     def __close_db_connection(self):
         if self.connection:
             self.connection.close()
+            self.connection = None
             logger.info("Соединение с PostgreSQL закрыто")
 
     def save_backup_file(self, filename, content, content_hash):
@@ -38,6 +39,43 @@ class DatabaseManager:
         except (Exception, Error) as error:
             cursor.connection.rollback()
             logger.error(f"Ошибка при работе с PostgreSql: {str(error)}")
+        finally:
+            cursor.close()
+            self.__close_db_connection()
+
+    def backup_file_exists(self, filename, content_hash):
+        try:
+            cursor = self.__create_cursor()
+            statement = "SELECT bf.id FROM backup_files bf WHERE bf.file_name = %s and bf.hash = %s;"
+            cursor.execute(statement, (filename, content_hash))
+            result = cursor.fetchone()
+            if result:
+                logger.info(f"Файл {filename} c хэшем {content_hash} найден в БД")
+                return True
+            return False
+        except (Exception, Error) as error:
+            logger.error(f"Ошибка при работе с PostgreSql: {str(error)}")
+        finally:
+            cursor.close()
+            self.__close_db_connection()
+
+    def delete_last_backup_files(self, files_number_to_skip):
+        try:
+            cursor = self.__create_cursor()
+            statement = "SELECT bf.id FROM backup_files bf ORDER BY bf.created_at DESC OFFSET %s;"
+            cursor.execute(statement, (str(files_number_to_skip)))
+            rows = cursor.fetchall()
+            if not rows:
+                logger.info(f"Не найдены файлы, которые нужно удалить")
+            else:
+                ids_to_delete = [str(row[0]) for row in rows]
+                cursor.execute("DELETE FROM backup_files WHERE id = ANY(%s::int[]);", (ids_to_delete,))
+                cursor.connection.commit()
+                logger.info(f"Файлов удалено: {len(ids_to_delete)}")
+        except (Exception, Error) as error:
+            logger.error(f"Ошибка при работе с PostgreSql: {str(error)}")
+            if rows:
+                cursor.connection.rollback()
         finally:
             cursor.close()
             self.__close_db_connection()
--- singularity_parser/main.py
+++ singularity_parser/main.py
@@ -4,7 +4,10 @@ import hashlib
 from backup_files_loader import find_backup_files, BackupFilesNotFoundError, BackupFilesLoadingError
 from singularity_parser.backup_files_repository import DatabaseManager
 
-path_to_backup_files = "C:\\Users\\79198\\AppData\\Roaming\\SingularityApp\\nedb-backup"
+#path_to_backup_files = "C:\\Users\\79198\\AppData\\Roaming\\SingularityApp\\nedb-backup"
+path_to_backup_files = "D:\\PythonProjects\\singularity-parser\\test_backup_files"
+
+MAX_BACKUP_FILES = 5
 
 
 if __name__ == '__main__':
@@ -13,7 +16,9 @@ if __name__ == '__main__':
         file_hash = hashlib.md5(str(backup_file.content).encode()).hexdigest()
         logger.info(f"Найден файл {backup_file.name}, MD5 хэш содержимого: {file_hash}")
         db_manager = DatabaseManager()
-        db_manager.save_backup_file(backup_file.name, str(backup_file.content), file_hash)
+        if not db_manager.backup_file_exists(backup_file.name, file_hash):
+            db_manager.save_backup_file(backup_file.name, str(backup_file.content), file_hash)
+        db_manager.delete_last_backup_files(MAX_BACKUP_FILES)
     except BackupFilesNotFoundError as e:
         logger.warning(str(e))
     except BackupFilesLoadingError as e:
